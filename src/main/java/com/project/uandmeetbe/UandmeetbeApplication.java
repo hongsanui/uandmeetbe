e98f6bdc2b24b3e42d64d18e637fa40f9b35f4c4
hongsanui <sakbal_@naver.com>
2022-08-17 18:27:41 +0900

    member 테이블 작성중

---
+++ src/main/java/com/project/uandmeetbe/UandmeetbeApplication.java
@@ -0,0 +1,13 @@
+package com.project.uandmeetbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UandmeetbeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UandmeetbeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/uandmeetbe/UandmeetbeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.uandmeetbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UandmeetbeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
